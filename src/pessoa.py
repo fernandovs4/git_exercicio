12f32f241464b8562212cbcc4a59cdd05ad58e00
Fernando <fervsantos3000@gmail.com>
2022-05-10 08:43:45 -0300

    estrutura inicial do projeto

---
+++ src/pessoa.py
@@ -0,0 +1,6 @@
+class Pesso():
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+    def fala_algo(self):
+        return f'Oi! Meu nome é {self.nome}'
---
+++ src/programa.py
@@ -0,0 +1,4 @@
+from pessoa import Pesso
+
+p1 = Pesso('joao', 30)
+p1.fala_algo()
